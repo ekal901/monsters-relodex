8763a5a213b94431240923ac9729b23ac6cb6e76
Damhee Kim <ekal.kdh@gmail.com>
2022-03-19 23:09:23 +0900

    use filter to search array

--- src/App.js
+++ src/App.js
@@ -8,25 +8,43 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: [
-        {
-          id: 'aaa',
-          name: 'mundo'
-        },
-        {
-          id: 'bbb',
-          name: 'ron'
-        },
-        {
-          id: 'ccc',
-          name: 'iceBear'
-        }
-      ]
+      monsters: []
     }
   }
+
+  // first time react render, only happends once 
+  // 1. constrctor -> 2. render -> 3. componentDidMount -> 4. render(state변화로 다시 render)
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then((response) => response.json())
+    .then((users) => this.setState(
+      () => {
+        return {monsters: users}
+      },
+      () => { // callback
+        console.log(this.state);
+      }
+    ))
+  }
+
   render() {
     return (
       <div className="App">
+        <input 
+          type="search" 
+          className='search-box' 
+          placeholder='search Monsters' 
+          onChange={(e) => {
+            const searchString = e.target.value.toLocaleLowerCase();
+            // when you modify array, you use new Array
+            const filteredMonsters = this.state.monsters.filter((monster, index) => {
+              return monster.name.toLocaleLowerCase().includes(searchString);
+            })
+            this.setState(() => {
+              return {monsters: filteredMonsters}
+            })
+          }} 
+        />
         {this.state.monsters.map((monster) => {
           return <div key={monster.id}><h1>{monster.name}</h1></div>
         })}
