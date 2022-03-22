9642ca73301cb008188d5b327619374228996066
Damhee Kim <ekal.kdh@gmail.com>
2022-03-22 21:27:42 +0900

    app class component -> functional component

--- src/App.js
+++ src/App.js
@@ -1,51 +1,34 @@
-import { Component } from 'react'
+import { useEffect, useState } from 'react'
 
 import './App.css';
 import CardList from './components/card-list/card-list.component';
 import SearchBox from './components/search-box/search-box.component';
 
-class App extends Component {
-  constructor() {
-    super();
+const App = () => {
+  const [searchField, setSearchField] = useState('');
+  const [monsters, setMonsters] = useState([]);
+  const [filteredMonsters, setFilterMonsters] = useState(monsters);
 
-    this.state = {
-      monsters: [],
-      searchField: ''
-    }
-  }
-
-  // first time react render, only happends once 
-  // 1. constrctor -> 2. render -> 3. componentDidMount -> 4. render(state변화로 다시 render)
-  componentDidMount() {
+  useEffect(() => {
+    console.log('effect fired');
     fetch('https://jsonplaceholder.typicode.com/users')
     .then((response) => response.json())
-    .then((users) => this.setState(
-      () => {
-        return {monsters: users}
-      },
-      () => { // callback
-        console.log(this.state);
-      }
-    ))
-  }
+    .then((users) => setMonsters(users))
+  }, []);
 
-  onSearchChange = (e) => {
-    const searchField = e.target.value.toLocaleLowerCase();
-    
-    this.setState(() => {
-      return {searchField}
-    })
-  }
-
-  render() {
-    // destructuring
-    const {monsters, searchField} = this.state;
-    const {onSearchChange} = this;
-
-    // when you modify array, you use new Array
-    const filteredMonsters = monsters.filter((monster, index) => {
+  useEffect(() => {
+    const newFilteredMonsters = monsters.filter((monster, index) => {
       return monster.name.toLocaleLowerCase().includes(searchField);
     })
+    setFilterMonsters(newFilteredMonsters);
+  }, [monsters, searchField])
+
+  const onSearchChange = (e) => {
+    const searchFieldString = e.target.value.toLocaleLowerCase();
+    setSearchField(searchFieldString);
+  }
+
+    
 
     return (
       <div className="App">
@@ -59,6 +42,5 @@ class App extends Component {
       </div>
     );
   }
-}
 
 export default App;
